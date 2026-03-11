99851a5d132e09fe6fb456b33d9c45b0d8861442
Yonaki51 <yonivandon@hotmail.com>
2026-03-11 14:53:13 +0100

    create.php skelet toegevoegd

--- dbcalls/menukaart/create.php
+++ dbcalls/menukaart/create.php
@@ -1,6 +1,18 @@
 <?php
-// create.php – Voegt een nieuw gerecht toe aan de 'menukaart'-tabel.
-// Vereist: $conn (PDO-object) via dbcalls/conn.php.
-// Verwacht POST-parameters: naam, beschrijving, prijs, allergenen, afbeelding.
+include("../conn.php");
 
-// TODO: implementeer het aanmaken van een nieuw menukaart-item
+$voornaam = $_POST['voornaam'];
+$achternaam = $_POST['achternaam'];
+
+// variabel met een SQL querry
+$sql = "INSERT INTO `personeel`(`voornaam`, `achternaam`) VALUES (:voornaam, :achternaam)";
+
+// preparestatement
+$stmt = $conn->prepare($sql);
+
+// binden van variabel
+$stmt->bindParam( ":voornaam", $voornaam );
+$stmt->bindParam(':achternaam', $achternaam);
+
+// execute on db
+$stmt->execute();
