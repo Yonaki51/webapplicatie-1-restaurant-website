485f64da2fb799961a7d90b819cbdf691ea5625c
Yonaki51 <yonivandon@hotmail.com>
2026-03-16 12:05:06 +0100

    update.php ingevuld met de data uit de database

--- dbcalls/menukaart/update.php
+++ dbcalls/menukaart/update.php
@@ -1,6 +1,34 @@
 <?php
-// update.php – Werkt een bestaand gerecht bij in de 'menukaart'-tabel.
-// Vereist: $conn (PDO-object) via dbcalls/conn.php.
-// Verwacht POST-parameters: id, naam, beschrijving, prijs, allergenen, afbeelding.
 
-// TODO: implementeer het bijwerken van een bestaand menukaart-item
+include("../conn.php");
+
+if (!isset($_POST['naam'], $_POST['categorie'], $_POST['beschrijving'], $_POST['allergenen'], $_POST['afbeelding'], $_POST['prijs'], $_POST['ID'])) {
+	exit;
+}
+
+$naam = $_POST['naam'];
+$categorie = $_POST['categorie'];
+$beschrijving = $_POST['beschrijving'];
+$allergenen = $_POST['allergenen'];
+$afbeelding = '/assets/img/' . $_POST['afbeelding'];
+$prijs = $_POST['prijs'];
+$ID = (int)$_POST['ID'];
+
+// variabel met een SQL querry
+$sql = "UPDATE `menukaart` SET `Naam` = :naam, `categorie` = :categorie, `Beschrijving` = :beschrijving, `Allergenen` = :allergenen, `Afbeelding` = :afbeelding, `Prijs` = :prijs WHERE ID = :ID";
+
+
+// Prepared statement voorkomt SQL-injectie
+$stmt = $conn->prepare($sql);
+
+// binden van variabel
+$stmt->bindParam(':naam', $naam);
+$stmt->bindParam(':categorie', $categorie);
+$stmt->bindParam(':beschrijving', $beschrijving);
+$stmt->bindParam(':allergenen', $allergenen);
+$stmt->bindParam(':afbeelding', $afbeelding);
+$stmt->bindParam(':prijs', $prijs);
+$stmt->bindParam(':ID', $ID);
+
+// Voer de query uit op de database
+$stmt->execute();
